181875e8145774c3f7d540e5b8336a9a70e16714
baibai25 <kamuy.camel.0225@gmail.com>
2023-10-31 23:16:09 +0900

    First commit

---
+++ src/script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // 画像のリスト
+  // TODO: ネット画像か、スクレイピングされた画像一覧にする
+  var imageList = ["images/image1.jpg", "images/image2.jpg"];
+
+  // ランダムな画像の選択
+  var randomIndex = Math.floor(Math.random() * imageList.length);
+  var randomImage = imageList[randomIndex];
+
+  // CSSで背景画像を設定
+  document.body.style.backgroundImage = "url('" + randomImage + "')";
+
+  // 日付と時刻を表示
+  var dateContainer = document.getElementById("date");
+  var timeContainer = document.getElementById("time");
+
+  function updateDateTime() {
+    var currentDate = new Date();
+    var options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      weekday: "long",
+    };
+    var formattedDate = currentDate.toLocaleDateString("en-US", options);
+    var formattedTime = currentDate.toLocaleTimeString();
+
+    timeContainer.textContent = formattedTime;
+    dateContainer.textContent = formattedDate;
+  }
+
+  updateDateTime();
+  setInterval(updateDateTime, 1000);
+});
