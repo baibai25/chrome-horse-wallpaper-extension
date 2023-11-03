9a0206d8131077e366d5726ac53d97322df5df90
baibai25 <kamuy.camel.0225@gmail.com>
2023-11-03 23:48:03 +0900

    Add: Save API Data when extention installed

--- src/background.js
+++ src/background.js
@@ -1,24 +1,40 @@
-// WARNING: Very slow
-chrome.runtime.onMessage.addListener(function (request, sender, callback) {
-  // 「ウェブアプリ」としてデプロイしているGASのURL
-  const gasUrl =
-    "https://script.google.com/macros/s/AKfycbzbQgwnBYmqWjXv8ILaFmtlKpbh7RL-v_HlkpRcC53cskyXEFx6GyxNxkhCkWN6rMKqwg/exec";
+// 初回インストールの検出
+chrome.runtime.onInstalled.addListener(function (details) {
+  if (details.reason === "install") {
+    // 初回インストール時の処理
+    const gasUrl =
+      "https://script.google.com/macros/s/AKfycbzbQgwnBYmqWjXv8ILaFmtlKpbh7RL-v_HlkpRcC53cskyXEFx6GyxNxkhCkWN6rMKqwg/exec";
 
-  fetch(gasUrl)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      // GASからのデータをリストとして格納
-      const urlList = data.map((item) => item.URL);
+    fetch(gasUrl)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        // GASからのデータをリストとして格納
+        const urlList = data.map((item) => item.URL);
 
-      // リストをcallback経由でcontent.jsに返す
-      callback(urlList);
-    })
-    .catch((error) => {
-      console.error("データの取得中にエラーが発生しました: ", error);
+        // リストを拡張機能の永続ストレージに保存
+        // chrome.storage.sync.set({ imageList: urlList }, function () {
+        chrome.storage.local.set({ imageList: urlList }, function () {
+          console.log("APIデータを保存しました");
+        });
+      })
+      .catch((error) => {
+        console.error("データの取得中にエラーが発生しました: ", error);
+      });
+  }
+});
+
+// 新しいタブを開いたときにデータを読み込み、content.jsに送信
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message === "get_data") {
+    // 永続ストレージからデータを取得
+    chrome.storage.local.get(["imageList"], function (result) {
+      const imageList = result.imageList || [];
+      sendResponse(imageList);
     });
 
-  // 非同期を同期的に扱うためのtrue
-  return true;
+    // 非同期を同期的に扱うためのtrue
+    return true;
+  }
 });
--- src/content.js
+++ src/content.js
@@ -1,15 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var imageList = []; // 画像のリストを初期化
-
-  // test.txt ファイルのパス
-  const fileUrl = "image_list.txt";
-
-  // ファイルをフェッチして画像リストを取得
-  fetch(fileUrl)
-    .then((response) => response.text())
-    .then((text) => {
-      // テキストを改行で分割し、空白行をフィルタリングして画像リストに追加
-      imageList = text.split("\n").filter((line) => line.trim() !== "");
+  chrome.runtime.sendMessage(
+    {
+      message: "get_data",
+    },
+    (imageList) => {
+      console.log(imageList);
 
       // ランダムな画像の選択
       var randomIndex = Math.floor(Math.random() * imageList.length);
@@ -17,10 +12,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
       // CSSで背景画像を設定
       document.body.style.backgroundImage = "url('" + randomImage + "')";
-    })
-    .catch((error) => {
-      console.error("Error while loading files: ", error);
-    });
+    }
+  );
 
   // 日付と時刻を表示
   var dateContainer = document.getElementById("date");
